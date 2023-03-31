42f19efb05f2766f866a9b01411bf1565cb81f79
jdahlke <jdahlke@aidminutes.com>
2023-03-31 13:56:58 +0200

    very basic, typenames

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,52 @@
-#[allow(dead_code)]
 use serde::{Deserialize, Serialize};
+use serde_json::{self, Value};
 use std::collections::HashMap;
-use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
 
-struct Definition {}
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct FhirRessource {}
+struct BasicType {
+    resource_type: String,
+}
+fn main() {
+    println!("Hello, world!");
+    read_and_write_types();
+}
 
-impl fmt::Display for FhirRessource {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self)
+fn read_and_write_types() {
+    let mut file =
+        File::open("./fhir-json-raw/definitions/fhir.schema.json").expect("Unable to open file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let definitions: HashMap<String, Value> = serde_json::from_str(&contents).unwrap();
+    let mut counter = 1;
+    for key in definitions {
+        if key.0 == "discriminator".to_owned() {
+            println!("found discriminator");
+            let resource_type = key.0;
+            let definition = key.1;
+
+            for (field, value) in definition.as_object().unwrap() {
+                if field == "mapping" {
+                    println!("found mapping");
+                    for (field, value) in value.as_object().unwrap() {
+                        let types = generate_rust_fhir_type(field, value);
+                        println!("{}", types);
+                    }
+                } else {
+                }
+            }
+        } else {
+            println!("not found");
+        }
+        counter += 1;
     }
 }
 
-fn read_and_write_types() -> Result<(), Box<dyn std::error::Error>> {
-    let mut file = File::open("./fhir-json-raw/definitions/fhir.schema.json")?;
-    let mut data = String::new();
-    file.read_to_string(&mut data)?;
-    let definitions: HashMap<String, FhirRessource> = serde_json::from_str(&data).unwrap();
-
-    for def in definitions {
-        println!("Generating type for {}", def);
-        let rust_type = generate_rust_fhir_type(&def);
-        let mut file = File::create(format!("./fhir-types/{}.rs", def.Can))?;
-        file.write_all(rust_type.as_bytes())?;
-    }
-    Ok(())
-}
-
-//this should generate our rust type files
-fn generate_rust_fhir_type(definition: &FhirRessource) -> String {
+fn generate_rust_fhir_type(resource_type: &str, definition: &Value) -> String {
     let mut fhir_type = String::new();
     fhir_type.push_str("use serde::{Deserialize, Serialize};\n");
     fhir_type.push_str("#[derive(Serialize, Deserialize, Debug)]\n");
-    fhir_type.push_str(&format!("pub struct {} {{", definition.resource_type));
-    fhir_type.push_str("}\n");
+    fhir_type.push_str(&format!("pub struct {} {{", resource_type));
+    fhir_type.push_str("\n}\n");
     fhir_type
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    read_and_write_types()?;
-    Ok(())
 }
